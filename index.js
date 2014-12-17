579b6b124f16564f03649f7e36616122ba575d32
red-s-bean <red_s_bean@163.com>
2014-12-17 11:20:25 +0800

    update

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-
+var fis = require('fis');
 
 module.exports = function(content, file, conf){
-
+    if(file.rExt == '.jpl'){
+        content = 'module.exports = "' + 
+            content.replace(/"/g, '\\"').replace(/[\r\n]/g, '') +
+            '";'; 
+        file.rExt = '.js';
+    }
+    return content;
 }
