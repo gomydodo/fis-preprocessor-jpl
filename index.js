c45284c95f3dda8b98d447ba55e9fde345406127
gomydodo <gomydodo@163.com>
2017-01-06 19:48:31 +0800

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fis = require('fis');
 module.exports = function(content, file, conf){
     if(file.rExt == '.jpl'){
         content = 'module.exports = "' + 
-            content.replace(/"/g, '\\"').replace(/[\r\n]/g, '') +
+            content.replace(/"/g, '\\"').replace(/[\r\n]/g, '').replace(/__uri\(([^)]+)\)/g, '" + __uri($1) + "') +
             '";'; 
         file.rExt = '.js';
     }
